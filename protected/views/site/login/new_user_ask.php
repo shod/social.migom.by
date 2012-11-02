a96e1f7a4b3ad7b79c325bdf980121bdba9b7cca
Evgeniy <evgeniy.kazak@gmail.com>
2012-11-02 13:48:55 +0300

    Login

--- protected/views/site/login/new_user_ask.php
+++ protected/views/site/login/new_user_ask.php
@@ -14,6 +14,6 @@
         }
     ?>
 	<p><?= Yii::t('Login', 'Вы сможете входить на сайт migom.by без ввода пароля через свой аккаунт <strong class="{class}">{name}</strong>', array('{class}' => $service, '{name}' => $login)); ?></p>
-    <button onclick="window.location('<?= $this->createUrl('site/login', array('service' => $service, 'user' => 'new', 'reg_ask' => 1)); ?>')"><?= Yii::t('Login', 'Я новый пользователь') ?></button>
-	<button onclick="window.location('<?= $this->createUrl('site/login', array('service' => $service, 'user' => 'haveALogin', 'reg_ask' => 1)); ?>')"><?= Yii::t('Login', 'У меня уже есть аккаунт') ?></button>
+    <button onclick="window.location = '<?= $this->createUrl('site/login', array('service' => $service, 'user' => 'new', 'reg_ask' => 1)); ?>'"><?= Yii::t('Login', 'Я новый пользователь') ?></button>
+	<button onclick="window.location = '<?= $this->createUrl('site/login', array('service' => $service, 'user' => 'haveALogin', 'reg_ask' => 1)); ?>'"><?= Yii::t('Login', 'У меня уже есть аккаунт') ?></button>
 </div>
