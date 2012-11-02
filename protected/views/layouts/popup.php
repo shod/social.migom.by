2d4a92be13500b6b8e4019876f5c2911a1234853
Evgeniy <evgeniy.kazak@gmail.com>
2012-11-02 12:56:22 +0300

    Login

--- protected/views/layouts/popup.php
+++ protected/views/layouts/popup.php
@@ -1,16 +1,18 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-	<head>
+<!doctype html>
+<html>
+<head>
+	<title></title>
+	<meta charset="utf-8">
+	<?php
+    Yii::app()->getClientScript()->registerCssFile('/css/default.css');
+    Yii::app()->clientScript->registerScriptFile('/js/default.js');
+    Yii::app()->clientScript->registerCoreScript('jquery');
+	Yii::app()->clientScript->registerCoreScript('jquery.ui');
+    ?>
+</head>
+<body>
 
-		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-		<meta http-equiv="Content-Language" content="ru" />
+    <?= $content ?>
 
-		<link rel="stylesheet" href="/css/reset.css" type="text/css" />
-		<link rel="stylesheet" href="/css/default.css" type="text/css" />
-		<link rel="stylesheet" href="/css/login.css" type="text/css" />
-	</head>
-
-	<body>
-			<?= $content ?>
-	</body>
+</body>
 </html>
