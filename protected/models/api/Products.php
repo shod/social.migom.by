7a6df82cb3abb15aa8cf808d04eef506b59c904b
USER <USER@USER-PC>
2012-11-02 16:45:27 +0300

    api first merge

--- protected/models/api/Products.php
+++ protected/models/api/Products.php
@@ -1,10 +1,32 @@
 <?php
 
-class Api_Products extends Api
+class Api_Products extends ERestDocument
 {
     public function attributeNames()
     {
         return array();
     }
+
+    public function getCollectionName()
+    {
+        return 'products';
+    }
+
+    /**
+	 * Get ERest component instance
+	 * By default it is ERest application component
+	 *
+	 * @return ERest
+	 * @since v1.0
+	 */
+	public function getRestComponent()
+	{
+		return $this->setRestComponent(Yii::app()->getComponent('migom'));
+	}
+
+    public static function model($className=__CLASS__)
+	{
+		return parent::model($className);
+	}
 
 }
