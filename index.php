056673ec65149935ccf55094a63bb91f21ccd5fc
Shod <shod@migom.by>
2013-02-11 10:14:30 +0300

    bild 11.02

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ $yiiEx  = dirname(__FILE__) . '/../core/YiiBaseEx.php';
 $config=dirname(__FILE__).'/protected/config/local.php';
 
 // remove the following lines when in production mode
-defined('YII_DEBUG') or define('YII_DEBUG',true);
+defined('YII_DEBUG') or define('YII_DEBUG',false);
 // specify how many levels of call stack should be shown in each log message
 defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL',3);
 
