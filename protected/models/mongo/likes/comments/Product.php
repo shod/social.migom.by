552aad3f4a7049847d32161e553c8f33c8aeabfa
Shod <shod@migom.by>
2013-03-07 11:58:34 +0300

    models comm

---
+++ protected/models/mongo/likes/comments/Product.php
@@ -0,0 +1,23 @@
+<?php
+
+class Likes_Comments_Article extends Likes
+{
+
+    /**
+     * This method have to be defined in every Model
+     * @return string MongoDB collection name, witch will be used to store documents of this model
+     */
+    public function getCollectionName()
+    {
+        return 'likes_comments_product';
+    }
+
+    /**
+     * This method have to be defined in every model, like with normal CActiveRecord
+     */
+    public static function model($className = 'Comments_Product')
+    {
+        return parent::model($className);
+    }
+
+}
