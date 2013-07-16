dfff48957e51569ba8081579bf0e9fcc39efef31
FTP <test2migomby@rasalas.neolocation.net>
2013-07-16 15:32:39 +0300

    mail merge

--- index.php
+++ index.php
@@ -28,7 +28,7 @@ require_once($yii);
 require_once($yiiEx);
 Yii::getLogger()->autoDump = true;
 Yii::getLogger()->autoFlush=1;
-Yii::setPathOfAlias("core", dirname(__FILE__).'/../core_ek');
+Yii::setPathOfAlias("core", dirname(__FILE__).'/../core');
 $yii = Yii::createWebApplication($config);
 spl_autoload_unregister(array('YiiBase', 'autoload'));
 spl_autoload_register(array('YiiBaseEx', 'autoload'));
