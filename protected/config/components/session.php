e17b0bb1638e07a3cbbff8f222a6b61f126f55b3
FTP <test2migomby@rasalas.neolocation.net>
2013-10-02 14:54:30 +0300

    [ek] from test

--- protected/config/components/session.php
+++ protected/config/components/session.php
@@ -3,6 +3,7 @@
 return array(
         'class' => 'CCacheHttpSession',
         'cacheID' => 'cache',
+		//'cookieParams' => array('domain' => '.social.test2.migom.by'),
         'timeout' => 3600 * 24 * 30,
         'autoStart' => true,
         //'cookieMode' => 'only',
